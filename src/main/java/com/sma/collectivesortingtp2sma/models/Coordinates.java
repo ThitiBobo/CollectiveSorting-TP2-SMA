c27a8b5e462e1058682482bda14175b2ccce2697
Baptiste MAGOUS <baptiste.magous@etu.univ-lyon1.fr>
2021-11-12 12:19:23 +0100

    DEV : Implements IElement, Cell and Coordinates

---
+++ src/main/java/com/sma/collectivesortingtp2sma/models/Coordinates.java
@@ -0,0 +1,30 @@
+package com.sma.collectivesortingtp2sma.models;
+
+public class Coordinates {
+    private int x, y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean equals(Coordinates coordinates){
+        return coordinates.getX() == this.getX() && coordinates.getY() == this.getY();
+    }
+}
---
+++ src/main/java/com/sma/collectivesortingtp2sma/models/IElement.java
@@ -0,0 +1,7 @@
+package com.sma.collectivesortingtp2sma.models;
+
+public interface IElement {
+    Coordinates getCoordinates();
+    void move(Coordinates coordinates);
+
+}
